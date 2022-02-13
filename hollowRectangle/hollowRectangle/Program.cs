067406bd637ad13710f5a896e78db213fa343bba
Pm Gohil <86161081+pmgohil45@users.noreply.github.com>
2022-02-13 15:17:09 +0530

    Add files via upload

---
+++ hollowRectangle/hollowRectangle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace hollowRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number : ");;
+            int i, j, m, n;
+            n = Convert.ToInt32(Console.ReadLine());
+            m = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= n; i++) 
+            { 
+                for (j = 1; j <= m; j++) 
+                {
+                    if (i == 1 || i == n || j == 1 || j == m)
+                    {
+                        Console.Write(" * ");
+                    }
+                    else
+                    {
+                        Console.Write("   ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
