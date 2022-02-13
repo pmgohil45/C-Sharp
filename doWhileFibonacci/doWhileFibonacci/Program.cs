4b5c5c2f721bc6bed8abbd549e2aa2b606435b99
Pm Gohil <86161081+pmgohil45@users.noreply.github.com>
2022-02-13 10:08:22 +0530

    Add files via upload

---
+++ doWhileFibonacci/doWhileFibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace doWhileFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0, n2 = 1, n3, count, i = 2;
+            Console.Write("Enter a number : ");
+            count = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n2 = n1;
+                n1 = n3;
+                i++;
+            } while (i != count) ;
+            Console.Read();
+        }
+    }
+}
---
+++ doWhileSeries1/doWhileSerise1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace doWhileSerise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             1 2 3 4 5....
+             */
+            int i=1, n;
+            Console.WriteLine("Enter a number : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                do
+                {
+                    Console.Write(i + " ");
+                    i++;
+                } while (i <= n);
+            } while (i == n);
+            Console.Read();
+        }
+    }
+}
