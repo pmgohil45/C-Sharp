0bbafca31befeae45ac349d9a11c400eede29c77
Pm Gohil <86161081+pmgohil45@users.noreply.github.com>
2022-02-13 10:15:30 +0530

    Add files via upload

---
+++ forFibonacci/forFibonacci/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace forFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number : ");
+            int n, i, j, a = 0, b = 1;
+            n = Convert.ToInt32(Console.ReadLine());
+            for(i=1; i <= n; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write(b+"\t");
+                    int temp = b;
+                    b = a + b;
+                    a = temp;
+                }
+                Console.WriteLine();
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ forLoop1/forLoop1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace forLoop1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, nm, j;
+            Console.Write("Enter a number : ");
+            nm = Convert.ToInt32(Console.ReadLine());
+            for (i = 0; i <= nm; i++)
+            {
+                for (j = 0; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
