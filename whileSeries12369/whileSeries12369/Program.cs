5a0ab73b5072dd2c41cd11fc696ee63bdb1de8b1
Pm Gohil <86161081+pmgohil45@users.noreply.github.com>
2022-02-13 10:21:13 +0530

    Add files via upload

---
+++ whileSeries12369/whileSeries12369/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace whileSeries12369
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1, a = 1, b = 2, n;
+            Console.WriteLine("Enter a number : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write(a + " ");
+            Console.Write(b + " ");
+            while (i <= n)
+            {
+                if (i % 2 == 1)
+                {
+                    a = a * 3;
+                    Console.Write(a + " ");
+                }
+                else
+                {
+                    b = b * 3;
+                    Console.Write(b + " ");
+                }
+                i++;
+            }
+            Console.Read();
+        }
+    }
+}
