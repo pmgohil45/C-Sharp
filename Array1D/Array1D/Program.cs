15ebf424be0c6a2e178c3aef2236015e5a673856
Pm Gohil <86161081+pmgohil45@users.noreply.github.com>
2022-02-13 10:07:20 +0530

    Add files via upload

---
+++ Array1D/Array1D/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Array1D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number for print array : ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            // 1D Array
+            Console.WriteLine("1D Array");
+            int[] arr = new int[n1];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = n1;
+            }
+            for (int i = 0; i < n1; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            
+            Console.Read();
+        }
+    }
+}
