f262c452af1cc8869428f55242c3dbfe4788e438
Pm Gohil <86161081+pmgohil45@users.noreply.github.com>
2022-02-13 10:06:39 +0530

    Add files via upload

---
+++ boxing-unboxing/boxing-unboxing/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace boxing_unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            object o = a; //boxing
+            Console.WriteLine("Boxing : " + a);
+            Console.WriteLine("Object : " + o);
+
+            int b = (int)o;//unboxing
+            Console.WriteLine("Unboxing : " + b);
+            Console.ReadLine(); 
+        }
+    }
+}
