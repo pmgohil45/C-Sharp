babb03164002cf5991492277d5dada67c448b3a1
Pm Gohil <86161081+pmgohil45@users.noreply.github.com>
2022-02-13 10:05:57 +0530

    Add files via upload

---
+++ printName/printName/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace printName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prakash Gohil");
+            Console.Read();
+        }
+    }
+}
